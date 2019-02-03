85e37513bedb63669db416d25602ba4402ec1017
unknown <orthogonaltrajectory@gmail.com>
2019-02-03 13:08:07 -0900

    Banner on main screen

--- gui.py
+++ gui.py
@@ -1,12 +1,25 @@
 from tkinter import *
 from tkinter import font
 
-root = Tk()
+windowWidth = 400
+windowHeight = 600
 
 class GameDetails(Frame):
-    def __init__(self):
-        Frame.__init__(self, master=root, Background="green")
-        self.configure(width=400, height=100) 
-        self.t = Label(self, text="Connect 4", font=font.Font(self, size=22, family='Arial'))
-        self.t.grid(sticky=W, pady=30)
+    def __init__(self, master):
+        Frame.__init__(self, master=root, bg="green")
+        self.configure(width=windowWidth, height=100) 
+        self.text = Label(self, text="Connect 4", font=font.Font(self, size=22, family='Arial'), background="green")
+        self.author = Label(self, text="Tristan Van Cise", font=font.Font(self, size=12, family='Arial'), background="green")
+        self.text.grid(sticky=W, pady=20)
+        self.author.grid(sticky=N)
 
+root = Tk()
+root.geometry("400x600")
+root.title("Connect 4")
+
+gameDetails = GameDetails(root)
+gameDetails.pack()
+
+# STUFF here
+
+root.mainloop()
